d66371dedee1a13cd9bec9e9af0e1725b341bd3c
Ksenofontov Gregory <geks333@yandex.ru>
2023-06-21 15:02:37 +0300

    Added teleportation

--- src/main/java/net/backhome/effect/TeleportEffect.java
+++ src/main/java/net/backhome/effect/TeleportEffect.java
@@ -3,6 +3,7 @@ package net.backhome.effect;
 import java.util.NoSuchElementException;
 
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.attribute.AttributeContainer;
 import net.minecraft.entity.effect.StatusEffect;
 import net.minecraft.entity.effect.StatusEffectCategory;
 import net.minecraft.entity.effect.StatusEffectInstance;
@@ -32,39 +33,38 @@ public class TeleportEffect extends StatusEffect {
         System.out.println(pLivingEntity.getPos());
     }
 
+    @Override
+    public void onRemoved(LivingEntity entity, AttributeContainer attributes, int amplifier) {
+        // Verify we are processing the use action on the logical server
+        if (entity instanceof ServerPlayerEntity){
+            // Cast to Server* class
+            ServerPlayerEntity serverPlayerEntity = (ServerPlayerEntity) entity;
+            ServerWorld serverWorld = serverPlayerEntity.getServerWorld();
 
+            if (serverWorld.getRegistryKey() !=  World.OVERWORLD)
+                serverWorld = serverWorld.getServer().getWorld(World.OVERWORLD);
+                
+            try {
+                // Get spawn postition
+                BlockPos spawn_position = serverPlayerEntity.getSpawnPointPosition();
+
+                // Calculate nearest suitable block to teleport
+                Vec3d suitable_pos = PlayerEntity.findRespawnPosition(serverWorld, spawn_position, serverPlayerEntity.getSpawnAngle(), false, false).get();
+                
+                // Teleport and play sound of Enderman
+                serverPlayerEntity.teleport(serverWorld, suitable_pos.getX(), suitable_pos.getY(), suitable_pos.getZ(), serverPlayerEntity.getYaw(), 1.0F);
+                serverWorld.playSound(null, spawn_position, SoundEvents.ENTITY_ENDERMAN_TELEPORT, SoundCategory.PLAYERS, 1.0F, 1.0F);
+                
+            } catch (NoSuchElementException e) { // There is no spawn point
+                // TODO: Add random teleport
+                // Calculate current position and plays there fail sound
+                BlockPos current_pos = new BlockPos((int) serverPlayerEntity.getX(), (int) serverPlayerEntity.getY(), (int) serverPlayerEntity.getZ());
+                serverWorld.playSound(null, current_pos, SoundEvents.BLOCK_DISPENSER_FAIL, SoundCategory.PLAYERS, 1.0F, 1.0F);
+            }            
+        }   
+    }
 
     // @Override
     // public TypedActionResult<ItemStack> use(World world, PlayerEntity playerEntity, Hand hand) {
-    //     // Verify we are processing the use action on the logical server
-    //     if (playerEntity instanceof ServerPlayerEntity){
-    //         // Cast to Server* class
-    //         ServerPlayerEntity serverPlayerEntity = (ServerPlayerEntity) playerEntity;
-    //         ServerWorld serverWorld = (ServerWorld) world;
 
-    //         if (serverWorld.getRegistryKey() !=  World.OVERWORLD)
-    //             serverWorld = serverWorld.getServer().getWorld(World.OVERWORLD);
-                
-    //         try {
-    //             // Get spawn postition
-    //             BlockPos spawn_position = serverPlayerEntity.getSpawnPointPosition();
-
-    //             // Calculate nearest suitable block to teleport
-    //             Vec3d suitable_pos = PlayerEntity.findRespawnPosition(serverWorld, spawn_position, serverPlayerEntity.getSpawnAngle(), false, false).get();
-                
-    //             // Teleport and play sound of Enderman
-    //             serverPlayerEntity.teleport(serverWorld, suitable_pos.getX(), suitable_pos.getY(), suitable_pos.getZ(), serverPlayerEntity.getYaw(), 1.0F);
-    //             serverWorld.playSound(null, spawn_position, SoundEvents.ENTITY_ENDERMAN_TELEPORT, SoundCategory.PLAYERS, 1.0F, 1.0F);
-                 
-    //         } catch (NoSuchElementException e) { // There is no spawn point
-    //             // Calculate current position and plays there fail sound
-    //             BlockPos current_pos = new BlockPos((int) serverPlayerEntity.getX(), (int) serverPlayerEntity.getY(), (int) serverPlayerEntity.getZ());
-    //             serverWorld.playSound(null, current_pos, SoundEvents.BLOCK_DISPENSER_FAIL, SoundCategory.PLAYERS, 1.0F, 1.0F);
-                
-    //             return TypedActionResult.fail(playerEntity.getStackInHand(hand));
-    //         }            
-    //     }   
-        
-    //     return TypedActionResult.success(playerEntity.getStackInHand(hand));
-    // }
 }
