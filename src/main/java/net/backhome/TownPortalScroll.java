a5c1fd85da143e54da3aeb8f3fd261978be81cb4
Ksenofontov Gregory <geks333@yandex.ru>
2023-06-21 07:30:17 +0300

    Add class for TP

---
+++ src/main/java/net/backhome/TownPortalScroll.java
@@ -0,0 +1,11 @@
+package net.backhome;
+
+import net.minecraft.item.Item;
+
+
+public class TownPortalScroll extends Item {
+    
+    public TownPortalScroll(Settings settings) {
+        super(settings);
+    }
+}
