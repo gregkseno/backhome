2330973f30e5f6d0360a6c8701065c5c6424962c
Ksenofontov Gregory <geks333@yandex.ru>
2023-06-21 06:54:33 +0300

    Add sample item

--- src/main/java/net/backhome/ExampleMod.java
+++ src/main/java/net/backhome/ExampleMod.java
@@ -1,22 +1,27 @@
 package net.backhome;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+
+import net.minecraft.item.Item;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class ExampleMod implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
     public static final Logger LOGGER = LoggerFactory.getLogger("backhome");
+
+
+	public static final Item TOWN_PORTAL_SCROLL = new Item(new FabricItemSettings());
 
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-
-		LOGGER.info("Hello Fabric world!");
+	
+		Registry.register(Registries.ITEM, new Identifier("backhome", "town_portal_scroll"), TOWN_PORTAL_SCROLL);
+		LOGGER.info("Mod initialized, new item added :)");
 	}
 }
